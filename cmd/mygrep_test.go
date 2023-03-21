62676f14089afce25ae45e70dcc9b44bc92997aa
Pradhvan <Pradhvanbisht@gmail.com>
2023-03-21 17:16:37 +0530

    Add test for mygrep.go
    * Add test when individually `-c` and `-o` flag is used with
      `FindSearchWord()`
    * Add test when all the flag simultaneously used with `FindSearchWord()`

--- cmd/mygrep_test.go
+++ cmd/mygrep_test.go
@@ -1,9 +1,11 @@
 package cmd_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/Pradhvan/gogrep/cmd"
+	"github.com/Pradhvan/gogrep/pkg/io"
 	"github.com/Pradhvan/gogrep/pkg/parseflag"
 	"github.com/stretchr/testify/assert"
 )
@@ -13,6 +15,7 @@ func TestFindSearchWord(t *testing.T) {
 		name   string
 		output []string
 		config parseflag.Config
+		count  int
 	}{
 		{
 			name:   "Test for search present in the file.",
@@ -48,7 +51,7 @@ func TestFindSearchWord(t *testing.T) {
 			},
 		},
 		{
-			name:   "Test for casesensitive search.",
+			name:   "Test for `-i` casesensitive search.",
 			output: []string{"testdata/data.txt: FOO"},
 			config: parseflag.Config{
 				IsCaseSensitive:   true,
@@ -56,6 +59,41 @@ func TestFindSearchWord(t *testing.T) {
 				OutputFile:        "",
 				CountSearchResult: false,
 				Args:              []string{"FOO", "testdata/data.txt"},
+			},
+		},
+		{
+			name:   "Test for `-c` with casesensitive search.",
+			output: []string{"testdata/data.txt: FOO"},
+			count:  1,
+			config: parseflag.Config{
+				IsCaseSensitive:   true,
+				CountBefore:       0,
+				OutputFile:        "",
+				CountSearchResult: true,
+				Args:              []string{"FOO", "testdata/data.txt"},
+			},
+		},
+		{
+			name:   "Test for `-c` without casesensitive search.",
+			count:  2,
+			output: []string{"testdata/data.txt: foobar", "testdata/data.txt: FOO"},
+			config: parseflag.Config{
+				IsCaseSensitive:   false,
+				CountBefore:       0,
+				OutputFile:        "",
+				CountSearchResult: true,
+				Args:              []string{"foo", "testdata/data.txt"},
+			},
+		},
+		{
+			name:   "Test for `-o`.",
+			output: []string{"testdata/data.txt: foobar", "testdata/data.txt: FOO"},
+			config: parseflag.Config{
+				IsCaseSensitive:   false,
+				CountBefore:       0,
+				OutputFile:        "output.txt",
+				CountSearchResult: false,
+				Args:              []string{"foo", "testdata/data.txt"},
 			},
 		},
 		{
@@ -91,12 +129,41 @@ func TestFindSearchWord(t *testing.T) {
 				Args:              []string{"program", "testdata/data.txt"},
 			},
 		},
+		{
+			name:   "Test for multiple flags usage.",
+			output: []string{"testdata/data.txt: this is a text here.", "testdata/data.txt: Another text goes here."},
+			count:  2,
+			config: parseflag.Config{
+				IsCaseSensitive:   true,
+				CountBefore:       1,
+				CountSearchResult: true,
+				OutputFile:        "output.txt",
+				Args:              []string{"Another", "testdata/data.txt"},
+			},
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+
 			r, _ := cmd.FindSearchWord(test.config)
 			assert.Equal(t, test.output, r.MatchText)
+
+			if test.config.CountSearchResult {
+				assert.Equal(t, test.count, r.Count)
+			}
+			// Check if output file exists
+			// and clean it up after the test is run.
+			if len(test.config.OutputFile) > 0 {
+				assert.Equal(t, true, r.MatchFileWrote)
+				outFileExists, err := io.CheckFileExists(test.config.OutputFile)
+				assert.Equal(t, nil, err)
+				assert.Equal(t, true, outFileExists)
+				if outFileExists {
+					err := os.Remove(test.config.OutputFile)
+					assert.Equal(t, nil, err)
+				}
+			}
 		})
 	}
 
